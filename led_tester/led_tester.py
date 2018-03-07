13f8595b5c7dd2cfcacc0709bc382d8d434a7283
PavineeKavanagh <pavinee.kavanagh@gmail.com>
2018-03-07 15:09:10 +0000

    simple tests passed

--- led_tester/led_tester.py
+++ led_tester/led_tester.py
@@ -1,20 +1,46 @@
 # -*- coding: utf-8 -*-
-
+from led_tester import utils
 """Main module."""
 class LEDTester:
     lights = None
     
-    def __init__(self, N):
-        self.lights = [[False]*size for _ in range(size)]
+    def __init__(self, N, instructions):
+        self.lights = [[False]*N for _ in range(N)]
+        self.instructions = instructions
+        self.operations(self)
     
-    def apply(self, instruction):
-        if instruction == switchOn
-            #switch on
-        elif instruction == switchOff
-            #switch off
-        elif instruction == toogle
+    def operations(self):
+        for i in self.instructions:
+            command, start, end = #regex here
+            self.apply(self, command, start, end)
+    
+    def apply(self, command, start, end):
+        if command == "turn on"
+            self.turnon(self, start, end)
+        elif command == 'turn off'
+            self.turnoff(self, start, end)
+        elif command == 'switch'
+            self.switch(self, start, end)
+            
+    def turnon(self, start, end):
+        for i in range():
+            for j in range():
+            
+        
+    def turnoff(self, start, end):
+        
+    def switch(self, start, end):
     
     def countOccupied(self):
         return count
     
     
+
+def main():
+    ifile = "C:/Users/Pavinee/led_tester/data/test_data.txt"
+    N, instructions = utils.parseFile(ifile)
+    matrix = LEDTester(N, instructions)
+
+
+if __name__=='__main__':
+    main()
--- tests/test_led_tester.py
+++ tests/test_led_tester.py
@@ -9,21 +9,23 @@ import pytest
 
 from click.testing import CliRunner
 
-from led_tester import led_tester
+#from led_tester import led_tester
 from led_tester import cli
 from led_tester import utils
 
-
-def test_command_line_interface():
-    ifile = "./data/test_data.txt"
-    N, instructions = utils.parseFile(ifile)
-    assert N is not None
-    assert None is not None
-
-def test_read_file():
-    ifile = "./data/test_data.txt"
-    N, instructions = utils.parseFile(ifile)
-    assert N == 10
-    assert instructions == ['turn on 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'switch 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'turn on 2,2 through 7,7\n']
-   
+class tests(unittest.TestCase):
+    
+    def test_command_line_interface(self):
+        ifile = "C:/Users/Pavinee/led_tester/data/test_data.txt"
+        N, instructions = utils.parseFile(ifile)
+        assert N is not None
+        assert instructions is not None
+    
+    def test_read_file(self):
+        ifile = "C:/Users/Pavinee/led_tester/data/test_data.txt"
+        N, instructions = utils.parseFile(ifile)
+        assert N == 10
+        print()
+        assert instructions == ['turn on 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'switch 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'turn on 2,2 through 7,7']
+       
     
